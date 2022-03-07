c28fdc89236e0263e1474fb456bba3ef8b0866f2
krizka974 <kevin.bruvry@gmail.com>
2022-03-07 23:17:05 +0800

    move_semantics, if, primitive_types, quizz1

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,8 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a = ["r";100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
@@ -13,3 +11,4 @@ fn main() {
         println!("Meh, I eat arrays like that for breakfast.");
     }
 }
+
